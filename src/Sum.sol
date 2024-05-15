cf273dbfbdc2bb8057e0686977ec9230ed2254f7
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-15 14:30:42 +0100

    updates on sum function

--- src/Sum.sol
+++ src/Sum.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 contract Contract {
     function sum(uint[5] calldata x) external pure returns (uint total) {
         for (uint i = 0; i < 5; i++) {
-            total += x[i];
+            total += x[i]; // add in sum of array
         }
     }
 }
