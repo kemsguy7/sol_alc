d0d3147a83f5f66999e6a8f6093f750aaac76114
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-15 14:52:49 +0100

    updates

--- src/Sum.sol
+++ src/Sum.sol
@@ -3,8 +3,16 @@ pragma solidity 0.8.20;
 
 contract Contract {
     function sum(uint[5] calldata x) external pure returns (uint total) {
+        //this function takes an array of 5 elements and returns the sum of the elements
         for (uint i = 0; i < 5; i++) {
             total += x[i]; // add in sum of array
+        }
+    }
+
+    //this function sums a dynamic array
+    function dynamicSum(uint[] calldata x) external pure returns (uint total) {
+        for (uint i = 0; i < x.length; i++) {
+            total += x[i];
         }
     }
 }
