c89f6f7db46cbed4d2015224b4f33baa643ea181
Matthew Idungafa <mattidungafa@gmail.com>
2024-05-16 19:23:39 +0100

    further updates on votes

--- src/struct.sol
+++ src/struct.sol
@@ -2,18 +2,29 @@
 pragma solidity 0.8.20;
 
 contract Contract {
-	enum Choices { Yes, No }
+    enum Choices {
+        Yes,
+        No
+    }
 
-	// TODO: create a vote struct and a public state variable
-	struct Vote {
-		Choices choice;
-		address voter;
-	}
+    // TODO: create a vote struct and a public state variable
+    struct Vote {
+        Choices choice;
+        address voter;
+    }
 
-	Vote  public vote;
+    // TODO: create a public state variable: an array of votes
+    Vote[] public votes;
 
-	function createVote(Choices choice) external {
-		// TODO: create a new vote
-		vote =  Vote(choice, msg.sender);
-	}
+    Vote public vote;
+
+    function createVote(Choices choice) external view returns (Vote memory) {
+        //Create an external, view function called createVote which takes Choices value as a parameter and returns an instance of type Vote.
+        //
+        return Vote(choice, msg.sender); //use the Choices value and the msg.sender as the values to create the vote.
+
+        // TODO: add a new vote to the array of votes state variable
+
+        votes.push(Vote(choice, msg.sender));
+    }
 }
